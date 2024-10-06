20b1b2a17ca906dca88a7691b08cd2b4ff9b740f
shaik-afridi-29 <afridishaik01@gmail.com>
2024-10-06 20:20:23 +0530

    My First Commit

---
+++ First.py
@@ -0,0 +1,21 @@
+class Car:
+
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Car: {self.year} {self.brand} {self.model}")
+    
+    def start(self):
+        print(f"The {self.brand} {self.model} is starting.")
+
+    def stop(self):
+        print(f"The {self.brand} {self.model} is stopping.")
+
+my_car = Car("Toyota", "Camry", 2021)
+
+my_car.display_info()
+my_car.start()
+my_car.stop()
